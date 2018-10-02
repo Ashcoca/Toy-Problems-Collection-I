c11971594c956a6af9a218d11fd96409208ab751
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-02 09:53:51 -0700

    Kinda finished

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,8 +10,40 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+ /*
+ I just realized I misread the instructions, I've been trying to keep track of the # of common chars
+ I'll have to come back and retry this exercise later
+ */
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  var letterCount = {};
+  var firstString = string.split('');
+  var secondString = string.split('');
+  var newStr = '';
+
+  for (var i = 0; i < firstString.length; i++) {
+    if (letterCount[firstString[i]] === undefined) {
+      letterCount[firstString[i]] = 1;
+    }
+    if (letterCount[firstString1[i]]) {
+      letterCount[firstString[i]] ++;
+    }
+  }
+  for (var j = 0; j < secondString.length; j++) {
+    if (letterCount[secondString[j]] === undefined) {
+      letterCount[secondString[j]] = 1;
+    }
+    if (letterCount[secondString[j]]) {
+      letterCount[secondString[j]] ++;
+    }
+  }
+  for (var key in letterCount) {
+    newStr += letterCount[key]
+  }
+  console.log(newStr)
+  return newStr;
+
+
 };
+
