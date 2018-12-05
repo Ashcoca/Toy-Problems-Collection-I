95cbe72307dcfa33b7c05ee5d1c8b3f5415eb584
Ashvin Coca <AshvinCoca@gmail.com>
2018-12-05 09:38:56 -0800

    Super inelegant solution, but it's also super awesome

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -1,6 +1,6 @@
 /*
 
-In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+In England the currency is made up of pound, £, and pence, p, and there are eight denominations in general circulation:
 
 1p piece
 2p piece
@@ -14,7 +14,7 @@ In England the currency is made up of pound, £, and pence, p, and there are eig
 It is possible to make £2 in the following way:
 
 1 * £1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
-How many different ways can £2 be made using any number of coins?
+How many different ways can £2 be made using any number of denominations?
 
 example usage of `makeChange`:
 
@@ -25,7 +25,39 @@ makeChange(2) === 2
 */
 
 var makeChange = function(total) {
-
+  // let newTotal = total * 100;
+  let count = 0;
+  let denominations = [
+    1,
+    2,
+    5,
+    10,
+    20,
+    50,
+    100,
+    200
+  ];
+  
+  //To solve this we want to start checking the LARGEST denominations first. Once combining the largest
+  //number exceeds the target, we use the next the lowest denomination, and so on until we reach the target.
+  //This problem could be solved in this awesome way:
+  for (var a = total; a >= 0; a -= denominations[denominations.length-1]) {
+    for (var b = a; b >= 0; b -= denominations[denominations.length-2]) {
+      for (var c = b; c >= 0; c -= denominations[denominations.length-3]) {
+        for (var d = c; d >= 0; d -= denominations[denominations.length-4]) {
+          for (var e = d; e >= 0; e -= denominations[denominations.length-5]) {
+            for (var f = e; f >= 0; f -= denominations[denominations.length-6]) {
+              for (var g = f; g >= 0; g -= denominations[denominations.length-7]) {
+                count++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  //Isn't that awesome? But it's also gross.
+  return count;
 };
 
 
