9eb041d78a1c1f04f4da8494a982c9ccad3f8bed
Ashvin Coca <AshvinCoca@gmail.com>
2018-12-05 23:12:51 -0800

    Mostly finished, understand the concept

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -23,9 +23,17 @@
  *
 */
 
-var bind = function(
-) {
-  // TODO: Your code here
+var bind = function(fn, obj) {
+  //get args array
+  let oldArgs = Array.prototype.slice.call(arguments, 2);
+  //call function
+  return () => {
+    let newArgs = Array.prototype.slice.call(arguments);
+    //concat both args arrays
+    let allArgs = oldArgs.concat(newArgs);
+    //Apply the args together
+    return fn.apply(obj, allArgs);
+  };
 };
 
 /*
@@ -53,7 +61,10 @@ var bind = function(
  *
 */
 
-Function.prototype.bind = function(
-) {
-  // TODO: Your code here
+//similar to last
+Function.prototype.bind = function(obj) {
+  //only 1 argument this time
+  let oldArgs = Array.prototype.slice.call(arguments, 1);
+  //probably same as last time from here
+
 };
